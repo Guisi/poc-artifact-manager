b5eaee322869633c2377bf3564934484d76b6bcf
Guisi <dsmi@gft.com>
2020-02-28 14:51:21 -0300

    Versão Inicial

---
+++ src/main/java/br/com/guisi/poc/pocartifactmanager/PocArtifactManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.guisi.poc.pocartifactmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocArtifactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocArtifactManagerApplication.class, args);
+	}
+
+}
